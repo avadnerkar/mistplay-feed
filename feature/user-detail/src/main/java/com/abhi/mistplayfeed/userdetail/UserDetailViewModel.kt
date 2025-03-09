0967b6b8e5fab3ef9454075a4362875007d80df2
Abhishek Vadnerkar <abhishek@enovlab.com>
2025-03-08 21:34:01 -0500

    Fix route params

--- feature/user-detail/src/main/java/com/abhi/mistplayfeed/userdetail/UserDetailViewModel.kt
+++ feature/user-detail/src/main/java/com/abhi/mistplayfeed/userdetail/UserDetailViewModel.kt
@@ -1,7 +1,9 @@
 package com.abhi.mistplayfeed.userdetail
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.toRoute
 import com.abhi.mistplayfeed.userdetail.domain.GetUserDetailPropsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
@@ -11,9 +13,11 @@ import javax.inject.Inject
 
 @HiltViewModel
 class UserDetailViewModel @Inject constructor(
-    getUserDetailProps: GetUserDetailPropsUseCase
+    getUserDetailProps: GetUserDetailPropsUseCase,
+    savedStateHandle: SavedStateHandle
 ) : ViewModel() {
-    val uiState: StateFlow<UserDetailState> = getUserDetailProps(1L).stateIn(
+    val route = savedStateHandle.toRoute<UserDetailRoute>()
+    val uiState: StateFlow<UserDetailState> = getUserDetailProps(route.userId).stateIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(),
         initialValue = UserDetailState.Loading
